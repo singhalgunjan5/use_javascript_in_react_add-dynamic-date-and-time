1ce6da6f903b993dfdc32b6138e7c8fb18ff221b
gunjan <singhalgunjan5@gmail.com>
2021-12-24 23:41:57 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import React from 'react'
+import ReactDom from 'react-dom';
+
+const currDate= new Date().toLocaleDateString();
+const currTime= new Date().toLocaleTimeString();
+ReactDom.render(
+<>
+<br></br>
+<p>
+<h1>Hello My name is gunjan.
+<br></br>
+<br></br>
+This is my second project on react.
+<br></br>
+<br></br>
+Purpose is to show Dynamic Date and Time </h1>
+</p>
+<h1>
+<p>Current Date is = {currDate} </p>
+<p>Current Time is = {currTime} </p>
+</h1>
+</>,
+document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
